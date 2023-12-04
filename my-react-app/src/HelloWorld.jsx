0069d5c9a2016aebc4ded454af6306742e6d90bb
Eric Muhwezi <muhwezi1000@gmail.com>
2023-12-04 23:57:44 +0300

    import HelloWorld Componet

--- my-react-app/src/HelloWorld.jsx
+++ my-react-app/src/HelloWorld.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function HelloWorld (){
-  return (
-    <div>Hello World Today</div>
-  )
+function HelloWorld() {
+    return (
+        <div>Hello World Today</div>
+    )
 }
 
 export default HelloWorld
