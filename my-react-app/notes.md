d31368399e4e6094a0f77a758fe19192eb178ea5
Eric Muhwezi <muhwezi1000@gmail.com>
2023-12-07 00:30:24 +0300

    add notes.md

---
+++ my-react-app/notes.md
@@ -0,0 +1,6 @@
+lesson-1: using CDNs
+lesson-2: installing Vite
+lesson-3:components and JSX and styling
+lesson-4: props 
+lesson-5: composition
+lesson-6: State and useState
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,20 +2,65 @@ import HelloWorld from './HelloWorld'
 import Card from './Card'
 import UserDetails from './UserDetails'
 
-function App() {
+function Article(props) {
   return (
     <>
-      <HelloWorld />
-
-      <Card>
-        <img src="https://example.com/image.png" alt="some Image" />
-        <UserDetails name="John" age="30" />
-      </Card>
-
-      <Card name="Jane" age="24" />
-
+      <h1>{props.title}</h1>
+      <img src={props.image} alt={props.title} width={props.size}></img>
     </>
-  )
-}
+  );
 
-export default App
+  function App() {
+
+    const users = [
+      {
+        id: 1,
+        name: 'John',
+        age: 30,
+        user_image: 'https://picsum.photos/id/236/200/300'
+      },
+      {
+        id: 2,
+        name: 'Jane',
+        age: 24,
+        user_image: 'https://picsum.photos/id/237/200/300'
+      },
+      {
+        id: 3,
+        name: 'Jack',
+        age: 32,
+        user_image: 'https://picsum.photos/id/238/200/300'
+      },
+      {
+        id: 4,
+        name: 'Jill',
+        age: 28,
+        user_image: 'https://picsum.photos/id/239/200/300'
+      },
+    ]
+
+    return (
+      <>
+        <HelloWorld />
+        {
+          users.map((user) => {
+            return (
+              <Card key={user.id}>
+                <img src={user.user_image} alt="some Image" />
+                <UserDetails name={user.name} age={user.age} />
+              </Card>
+            )
+          })
+        }
+        <Article
+          details={{
+            title: 'Morse Code: A visual guide',
+            image: 'https://i.imgur.com/5OluoVs.png'
+          }}
+          size={100}
+        />
+      </>
+    )
+  }
+
+  export default App
